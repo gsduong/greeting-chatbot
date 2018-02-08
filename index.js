e5f1ea59734db377361c5d3a1bf1ee946365f766
gsduong <nguyenduong.ict.hust@gmail.com>
2018-02-09 04:53:29 +0700

    Add sender action

--- index.js
+++ index.js
@@ -31,6 +31,9 @@ app.post('/webhook', (req, res) => {
 			let sender_psid = webhook_event.sender.id;
 			console.log('Sender PSID: ' + sender_psid);
 
+			// SenderAction ...
+			sendTypingAction(sender_psid);
+
 			// Check if the event is a message or postback and
 			// pass the event to the appropriate handler function
 			if (webhook_event.message) {
@@ -106,7 +109,6 @@ function handlePostback(sender_psid, received_postback) {
 
 	// Get the payload for the postback
 	let payload = received_postback.payload;
-
 	// Set the response based on the postback payload
 	if (payload === 'yes') {
 		response = {
@@ -144,6 +146,31 @@ function callSendAPI(sender_psid, response) {
 			console.log('message sent!')
 		} else {
 			console.error("Unable to send message:" + err);
+		}
+	});
+}
+
+function sendTypingAction(sender_psid) {
+
+	let request_body = {
+		"recipient": {
+			"id": sender_psid
+		},
+		"sender_action": "typing_on"
+	}
+	// Send the HTTP request to the Messenger Platform
+	request({
+		"uri": "https://graph.facebook.com/v2.6/me/messages",
+		"qs": {
+			"access_token": process.env.PAGE_ACCESS_TOKEN
+		},
+		"method": "POST",
+		"json": request_body
+	}, (err, res, body) => {
+		if (!err) {
+			console.log('SenderAction sent!')
+		} else {
+			console.error("Unable to send sender action:" + err);
 		}
 	});
 }
